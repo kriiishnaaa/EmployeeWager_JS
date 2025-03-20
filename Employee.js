c09b58c53bfb23d3b5714a0a3fad8380a626a6b9
Krishna <krishnaag866@gmail.com>
2025-03-20 15:07:30 +0530

    checked employee presence

---
+++ Employee.js
@@ -0,0 +1,10 @@
+const present = 1;
+
+let isPresent = Math.floor(Math.random()*10)%2;
+
+if(isPresent == present){
+    console.log("Employee is Present");
+}
+else{
+    console.log("Employee is not Present");
+}
